b4f15b590cd24d04091a2768d9c3e11ca1d3b5e7
Olivia Weston <olivia.h.weston@gmail.com>
2022-08-01 11:33:08 +0100

    Commented out pun pragma (LSP is shouting at me)

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NamedFieldPuns        #-}
+-- {-# LANGUAGE NamedFieldPuns        #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DeriveAnyClass        #-}
 {-# LANGUAGE OverloadedStrings     #-}
